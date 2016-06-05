a017bb4092694601a4351aa9ef353cdbd8019ed8
lulzyyy <agar5@o2.pl>
2016-06-05 23:31:35 +0200

    Add files via upload

---
+++ src/gamemodes/Experimental.js
@@ -0,0 +1,138 @@
+var FFA = require('./FFA'); // Base gamemode
+var Cell = require('../entity/Cell');
+var Food = require('../entity/Food');
+var Virus = require('../entity/Virus');
+var VirusFeed = require('../entity/Virus').prototype.feed;
+var MotherCell = require('../entity/MotherCell');
+
+function Experimental() {
+    FFA.apply(this, Array.prototype.slice.call(arguments));
+
+    this.ID = 2;
+    this.name = "Experimental";
+    this.specByLeaderboard = true;
+
+    // Gamemode Specific Variables
+    this.nodesMother = [];
+    this.tickMother = 0;
+    this.tickMotherS = 0;
+
+    // Config
+    this.motherCellMass = 222;
+    this.motherUpdateInterval = 50; // How many ticks it takes to update the mother cell (1 tick = 50 ms)
+    this.motherSpawnInterval = 100; // How many ticks it takes to spawn another mother cell - Currently 5 seconds
+
+    this.motherMinAmount = 6;
+
+}
+
+module.exports = Experimental;
+Experimental.prototype = new FFA();
+
+// Gamemode Specific Functions
+
+Experimental.prototype.updateMotherCells = function(gameServer) {
+    for (var i in this.nodesMother) {
+        var mother = this.nodesMother[i];
+
+        // Checks
+        mother.update(gameServer);
+        mother.checkEat(gameServer);
+    }
+};
+
+Experimental.prototype.spawnMotherCell = function(gameServer) {
+    // Checks if there are enough mother cells on the map
+    if (this.nodesMother.length < this.motherMinAmount) {
+        // Spawns a mother cell
+        var pos = gameServer.getRandomPosition();
+
+        // Check for players
+        for (var i = 0; i < gameServer.nodesPlayer.length; i++) {
+            var check = gameServer.nodesPlayer[i];
+
+            var r = check.getSize(); // Radius of checking player cell
+
+            // Collision box
+            var topY = check.position.y - r;
+            var bottomY = check.position.y + r;
+            var leftX = check.position.x - r;
+            var rightX = check.position.x + r;
+
+            // Check for collisions
+            if (pos.y > bottomY) {
+                continue;
+            }
+
+            if (pos.y < topY) {
+                continue;
+            }
+
+            if (pos.x > rightX) {
+                continue;
+            }
+
+            if (pos.x < leftX) {
+                continue;
+            }
+
+            // Collided
+            return;
+        }
+
+        // Spawn if no cells are colliding
+        var m = new MotherCell(gameServer.getNextNodeId(), null, pos, this.motherCellMass);
+        gameServer.addNode(m);
+    }
+};
+
+// Override
+
+Experimental.prototype.onServerInit = function(gameServer) {
+    // Called when the server starts
+    gameServer.run = true;
+    var mapSize = gameServer.config.borderLeft + gameServer.config.borderRight +
+        gameServer.config.borderTop + gameServer.config.borderRight;
+
+    this.motherMinAmount = Math.ceil(mapSize / 3194.382825); // 7 mother cells for agar.io map size
+    gameServer.lleaderboard = true;
+    // Special virus mechanics
+    Virus.prototype.feed = function(feeder, gameServer) {
+        gameServer.removeNode(feeder);
+        // Pushes the virus
+        this.setAngle(feeder.getAngle()); // Set direction if the virus explodes
+        this.moveEngineTicks = 10; // Amount of times to loop the movement function
+        this.moveEngineSpeed = 28;
+
+        var index = gameServer.movingNodes.indexOf(this);
+        if (index == -1) {
+            gameServer.movingNodes.push(this);
+        }
+    };
+
+    // Override this
+    gameServer.getRandomSpawn = gameServer.getRandomPosition;
+};
+
+Experimental.prototype.onTick = function(gameServer) {
+    // Mother Cell updates
+    this.updateMotherCells(gameServer);
+
+    // Mother Cell Spawning
+    if (this.tickMotherS >= this.motherSpawnInterval) {
+        this.spawnMotherCell(gameServer);
+        this.tickMotherS = 0;
+    } else {
+        this.tickMotherS++;
+    }
+};
+
+Experimental.prototype.onChange = function(gameServer) {
+    // Remove all mother cells
+    for (var i in this.nodesMother) {
+        gameServer.removeNode(this.nodesMother[i]);
+    }
+    // Add back default functions
+    Virus.prototype.feed = VirusFeed;
+    gameServer.getRandomSpawn = require('../GameServer').prototype.getRandomSpawn;
+};
---
+++ src/gamemodes/index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  Mode: require('./Mode'),
+  FFA: require('./FFA'),
+  Teams: require('./Teams'),
+  Experimental: require('./Experimental'),
+};
+
+var get = function (id) {
+  var mode;
+  switch (id) {
+    case 1: // Teams
+      mode = new module.exports.Teams();
+      break;
+    case 2: // Experimental
+      mode = new module.exports.Experimental();
+      break;
+    default: // FFA is default
+      mode = new module.exports.FFA();
+      break;
+  }
+  return mode;
+};
+
+module.exports.get = get;
