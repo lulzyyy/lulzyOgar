4ff2cae659bb1c2c83f939b0058e121640c2de4b
lulzyyy <agar5@o2.pl>
2016-06-06 22:48:10 +0200

    Update GameServer.js
    server is full alert, ban alert

--- src/GameServer.js
+++ src/GameServer.js
@@ -633,12 +633,6 @@ GameServer.prototype.start = function() {
 
     function connectionEstablished(ws) 
 	{
-        if (this.clients.length >= this.config.serverMaxConnections) 
-		{ // Server full, do not disconnect player because he will be trying to connect causing flooding
-				return;
-        }
-        
-
         function close(error) {
             var client = this.socket.playerTracker;
 			
@@ -659,8 +653,13 @@ GameServer.prototype.start = function() {
 	   // do not disconnect banned player because he will be trying to connect causing flooding
        if (this.banned.indexOf(ws._socket.remoteAddress) != -1) // Banned
 		{
-			//console.log("\u001B[33mClient " + ws._socket.remoteAddress + ", tried to connect but is banned!\u001B[0m");
+			console.log("\u001B[33mClient " + ws._socket.remoteAddress + ", tried to connect but is banned!");
 			ws.sendPacket(new Packet.SendAlert("You're banned."));
+		}
+		else if (this.clients.length >= this.config.serverMaxConnections)
+		{
+			console.log("\u001B[33mClient " + ws._socket.remoteAddress + ", tried to connect but server is full!");
+			ws.sendPacket(new Packet.SendAlert("Server is full."));
 		}
 		else // player isn't banned and server isn't full so create a new client
 		{
